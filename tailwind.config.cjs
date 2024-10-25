3e2f6c815109b27d49b52821cdbf4dfb7101943f
João Santos <jpnsantos14@gmail.com>
2024-10-25 15:00:57 +0100

    header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		conainter: {
+		container: {
 			center: true,
 			screens: {
 				"2xl": "1400px",
@@ -12,35 +12,16 @@ module.exports = {
 				sans: ['Inter', 'sans-serif'],
 			},
 			colors: {
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary))",
-					foreground: "hsl(var(--secondary-foreground))",
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-			}
-		},
-		borderRadius: {
-			xl: `calc(var(--radius) + 4px)`,
-			lg: `var(--radius)`,
-			md: `calc(var(--radius) - 2px)`,
-			sm: "calc(var(--radius) - 4px)",
+				background: "#fff",
+				foreground: "#191c1f",
+				muted: "#8d969e",
+			},
+			borderRadius: {
+				xl: `calc(var(--radius) + 4px)`,
+				lg: `var(--radius)`,
+				md: `calc(var(--radius) - 2px)`,
+				sm: "calc(var(--radius) - 4px)",
+			},
 		},
 	},
 	plugins: [],
