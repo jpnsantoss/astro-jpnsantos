b6cd8b4b5865248536ccbc7f42d98651ccfa1cef
João Santos <jpnsantos14@gmail.com>
2024-10-25 13:46:10 +0100

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+import sanity from "@sanity/astro";
+
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    // 👇 Update these lines
+    sanity({
+      projectId: "dbowprqv",
+      dataset: "production",
+      useCdn: false, // for static builds
+    }),
+  ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,47 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		conainter: {
+			center: true,
+			screens: {
+				"2xl": "1400px",
+			}
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+			},
+			colors: {
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+			}
+		},
+		borderRadius: {
+			xl: `calc(var(--radius) + 4px)`,
+			lg: `var(--radius)`,
+			md: `calc(var(--radius) - 2px)`,
+			sm: "calc(var(--radius) - 4px)",
+		},
 	},
 	plugins: [],
 };
