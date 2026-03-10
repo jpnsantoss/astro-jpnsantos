1919c2c690d562429dc45a166d2900bfecd37f9b
João Santos <jpnsantos14@gmail.com>
2026-03-10 12:33:45 +0000

    deployment adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
-
+import node from '@astrojs/node';
 import react from "@astrojs/react";
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({     
+    mode: 'standalone',
+  }),
   integrations: [tailwind(), sanity({
     projectId: "dbowprqv",
     dataset: "production",
